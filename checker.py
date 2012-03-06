57b85ed49a90509f6efab2faf40be77428742470
ws3389 <willsmith3389@gmail.com>
2012-03-07 00:00:03 +0800

    edit appid;add cron;add email func

--- checker.py
+++ checker.py
@@ -2,63 +2,87 @@ import webapp2
 import urllib
 
 from google.appengine.api import urlfetch
+from google.appengine.api import mail
 
+FORM_FIELDS = {'emailAddress': 'oran2ge', 'pswd': '123654'}
+FORM_DATA = urllib.urlencode(FORM_FIELDS)
+URL_HOMEPAGE = 'http://www.swagbucks.com/'
+URL_LOGIN = 'http://www.swagbucks.com/?cmd=sb-login&from=%2F%3Fcmd%3Dhome'
+HEADERS = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.11 Chrome/17.0.963.65',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset': 'UTF-8,*;q=0.5',
+            'Accept-Encoding': 'gzip,deflate,sdch',
+            'Accept-Language': 'en-US',
+            'Proxy-Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded',
+          }
+
+PREFIX = '<span class="user">'
+POSTFIX = '</span>You\'ve won'
+LUCKYNAME = 'WS3389'
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        form_fields = {"emailAddress": "oran2ge", "pswd": "123654"}
-        form_data = urllib.urlencode(form_fields)
-        url_homepage = 'http://www.swagbucks.com/'
-        url_login = 'http://www.swagbucks.com/?cmd=sb-login&from=%2F%3Fcmd%3Dhome'
-
-        #fetch homepage
-        result_homepage = urlfetch.fetch(url=url_homepage,
+        #get homepage
+        html_homepage = urlfetch.fetch(url=URL_HOMEPAGE,
                             headers={
-                                    'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.65',
-                                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                                    'Accept-Charset': 'UTF-8,*;q=0.5',
-                                    'Accept-Encoding': 'gzip,deflate,sdch',
-                                    'Accept-Language': 'en-US',
-                                    'Proxy-Connection': 'keep-alive'
-                                })
-        if result_homepage.status_code == 200:
-#            print self.response.out.write('home:' + str(result_homepage.headers) + '\n')
-            result_redirect = urlfetch.fetch(url=url_login, payload=form_data, method=urlfetch.POST,
+                                    'Accept': HEADERS['Accept'],
+                                    'Accept-Charset': HEADERS['Accept-Charset'],
+                                    'Accept-Encoding': HEADERS['Accept-Encoding'],
+                                    'Accept-Language': HEADERS['Accept-Language'],
+                                    'Proxy-Connection': HEADERS['Proxy-Connection'],
+                                    'User-Agent': HEADERS['User-Agent'],
+                                    })
+
+        #Cookies are not handled upon redirection, so set follow_redirects false
+        if html_homepage.status_code == 200:
+            html_redirect = urlfetch.fetch(url=URL_LOGIN, payload=FORM_DATA, method=urlfetch.POST,
                                 follow_redirects=False,
                                 headers={
-                                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                                    'Accept-Charset': 'UTF-8,*;q=0.5',
-                                    'Accept-Encoding': 'gzip,deflate,sdch',
-                                    'Proxy-Connection': 'keep-alive',
-                                    'Accept-Language': 'en-US',
-                                    'Origin': 'http://www.swagbucks.com',
-                                    'Content-Type': 'application/x-www-form-urlencoded',
-                                    'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.65',
-                                    'Cookie': result_homepage.headers['Set-Cookie'],
-                                    'P3P': result_homepage.headers['P3P'],
-                                    'Referer': url_homepage})
-
-            if result_redirect.status_code == 302:
-#                print self.response.out.write('302:' + str(result_redirect.headers) + '\n')
-#                print self.response.out.write('cookie:' + result_homepage.headers['Set-Cookie'] + result_redirect.headers['Set-Cookie'] + '\n')
-                result_home = urlfetch.fetch(url=result_redirect.headers['Location'],
+                                    'Accept': HEADERS['Accept'],
+                                    'Accept-Charset': HEADERS['Accept-Charset'],
+                                    'Accept-Encoding': HEADERS['Accept-Encoding'],
+                                    'Accept-Language': HEADERS['Accept-Language'],
+                                    'Content-Type': HEADERS['Content-Type'],
+                                    'Cookie': html_homepage.headers['Set-Cookie'],
+                                    'Origin': URL_HOMEPAGE,
+                                    'Proxy-Connection': HEADERS['Proxy-Connection'],
+                                    'P3P': html_homepage.headers['P3P'],
+                                    'User-Agent': HEADERS['User-Agent'],
+                                    'Referer': URL_HOMEPAGE
+                                    })
+            #If Login Succefully
+            if html_redirect.status_code == 302:
+                html_home = urlfetch.fetch(url=html_redirect.headers['Location'],
                                 headers={
-                                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                                    'Accept-Charset': 'UTF-8,*;q=0.5',
-                                    'Accept-Encoding': 'gzip,deflate,sdch',
-                                    'Proxy-Connection': 'keep-alive',
-                                    'Accept-Language': 'en-US',
-                                    'P3P': result_homepage.headers['P3P'],
-                                    'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.65',
-                                    'Cookie': result_redirect.headers['Set-Cookie'],
-                                    'Referer': url_homepage})
-                prefix = '<span class="user">'
-                postfix = '</span>You\'ve won'
-                content = result_home.content
-                name = content[(content.index(prefix)+len(prefix)):content.index(postfix)]
-                
-                #TODO send email
-                print self.response.out.write(name)
+                                    'Accept': HEADERS['Accept'],
+                                    'Accept-Charset': HEADERS['Accept-Charset'],
+                                    'Accept-Encoding': HEADERS['Accept-Encoding'],
+                                    'Accept-Language': HEADERS['Accept-Language'],
+                                    'Cookie': html_redirect.headers['Set-Cookie'],
+                                    'Proxy-Connection': HEADERS['Proxy-Connection'],
+                                    'P3P': html_homepage.headers['P3P'],
+                                    'User-Agent': HEADERS['User-Agent'],
+                                    'Referer': URL_HOMEPAGE
+                                    })
+                content = html_home.content
+
+                name = content[(content.index(PREFIX)+len(PREFIX)):content.index(POSTFIX)].upper()
+                #Send email
+                if name == LUCKYNAME:
+                    Emailme()                    
+
+                print self.response.out.write('LuckyMan:' + name)
+
+def Emailme():
+    message = mail.EmailMessage(sender='no-rely@code-checker.appspotmail.com',
+                                subject='Swagbuck Winner!')
+
+    message.to = 'niuben0405@gmail.com'
+    message.body = 'See the subject'
+
+    message.send()        
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
